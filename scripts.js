88e64d5c7a792687c9ae77fa5d6b479bdbf32e18
Steven Coleman <stevencoleman@Stevens-Laptop.local>
2024-03-20 16:33:31 -0700

    second commit

--- scripts.js
+++ scripts.js
@@ -1,50 +1,93 @@
 const board = document.getElementById("board");
 const answer = document.getElementById("answer");
+let columnOne = document.querySelectorAll(".column-one");
+let columnTwo = document.querySelectorAll(".column-two");
+columnTwo.forEach((element) => {
+  element.setAttribute("contenteditable", "true");
+});
+columnOne.forEach((element) => {
+  element.setAttribute("contenteditable", "true");
+});
+let first = "";
 let rows = 6;
 let columns = 5;
 let wordOfDay;
 const button = document.querySelector(".button");
-let tile = document.querySelector(".tile input");
 const URL = "https://words.dev-apis.com/word-of-the-day?random=1";
 async function fetchData() {
   const response = await fetch(URL);
   const data = await response.json();
   wordOfDay = data.word;
-  wordOfDay = wordOfDay.toUpperCase();
+  console.log(wordOfDay);
 }
 fetchData();
-// setTimeout(() => {
-//   document.querySelector("h1").innerHTML = wordOfDay;
-
-// }, 5000);
-
-let num = [];
-// function getAnswer() {
-//   for (let i = 1; i <= columns; i++) {
-//     num.push(i);
-//   }
-//   num.forEach((val) => {
-//     val = document.querySelector("h1").innerHTML = "column ";
-//     document.body.append(val);
-//   });
-//   console.log(num);
-// }
-//
-
-function getColumn() {
-  for (let i = 0; i <= columns - 1; i++) {
-    let tile = document.createElement("span");
-    tile.classList.add("tile");
-    tile.setAttribute("contenteditable", "true");
-    tile.addEventListener("keydown", function (event) {
-      if (tile.innerHTML.length >= 1 && event.key !== "Backspace") {
-        event.preventDefault();
-      }
-      console.log(tile.innerHTML);
-      console.log(tile.innerHTML.length);
-    });
-    board.append(tile);
+function firstGuess() {
+  let list = [];
+  for (let i = 0; i <= columnOne.length - 1; i++) {
+    list.push(columnOne[i].innerHTML);
+  }
+  console.log(list.length);
+  first = list.join("");
+}
+function highlightGreen() {
+  for (let i = 0; i <= columnOne.length - 1; i++) {
+    columnOne[i].classList.add("green");
+  }
+}
+function highlightYellow() {
+  for (let i = 0; i <= columnOne.length - 1; i++) {
+    columnOne[i].classList.add("yellow");
   }
 }
 
-getColumn();
+function isHighlightYellow() {
+  function getLetterCounter(word) {
+    let letterCounter = {};
+    for (let i = 0; i <= word.length - 1; i++) {
+      let char = word[i];
+      let prevCount = letterCounter[char] ?? 0;
+      letterCounter[char] = prevCount + 1;
+    }
+    return letterCounter;
+  }
+  const firstCounter = getLetterCounter(first);
+  const wordOfDayCounter = getLetterCounter(wordOfDay);
+  console.log(firstCounter);
+  console.log(wordOfDayCounter);
+
+  // let firstCounterValue;
+  // let wordOfDayCounterValue;
+  // wordOfDayCounterValue = Object.keys(wordOfDayCounter).join("");
+  // firstCounterValue = Object.keys(firstCounter).join("");
+
+  const firstValue = Object.keys(firstCounter);
+  const wordOfDayValue = Object.keys(wordOfDayCounter);
+
+  let foundMatch = false;
+  wordOfDayValue.some((value) => {
+    if (firstValue.includes(value)) {
+      foundMatch = true;
+      return true;
+    }
+  });
+  if (foundMatch) {
+    document.querySelectorAll(".column-one").forEach((element) => {
+      element.style.backgroundColor = "yellow";
+    });
+  }
+  return foundMatch;
+}
+button.addEventListener("click", () => {
+  firstGuess();
+  if (first == wordOfDay) {
+    highlightGreen();
+    console.log("TRUE");
+    return;
+  } else if (wordOfDay !== first) {
+    if (isHighlightYellow()) {
+      highlightYellow();
+    }
+
+    console.log("Not Quite");
+  }
+});
